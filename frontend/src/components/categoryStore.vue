f2e29a3d9255df5c9c3beb9be2845af1fbda8fbd
jangchangwan <tktkd6755@gmail.com>
2022-07-22 13:52:19 +0900

    update kakaoMap

--- frontend/src/components/categoryStore.vue
+++ frontend/src/components/categoryStore.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <h3>가게 카테고리입니다</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p>아이콘</p>
+        <p>1인분</p>
+      </div>
+      <div class="col">
+        <p>아이콘</p>
+        <p>돈까스◾회◾일식</p>
+      </div>
+      <div class="col">
+        <p>아이콘</p>
+        <p>중식</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>아이콘</p>
+        <p>카페◾디저트</p>
+      </div>
+      <div class="col">
+        <p>아이콘</p>
+        <p>분식</p>
+      </div>
+      <div class="col">
+        <p>아이콘</p>
+        <p>찜◾탕◾찌개</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +37,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ .container {
+  width: 360px;
+  height: 280px;
+ }
+ .col {
+  font-size: 15px;
+ }
 </style>
