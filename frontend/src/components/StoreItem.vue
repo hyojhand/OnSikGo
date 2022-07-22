373439d2c539ae3b4da61ce689d13967c3b09470
jangchangwan <tktkd6755@gmail.com>
2022-07-22 17:09:40 +0900

    update shop

---
+++ frontend/src/components/StoreItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="store-container">
+    <div class="row">
+      <!-- 가게 이미지 -->
+      <div class="col-2">이미지</div>
+      <!-- 가게 설명 -->
+      <div class="store col">
+        <p class="store-name">이마트 24</p>
+        <p class="store-prediction">300m, 도보로 3분</p>
+      </div>
+      <!-- 물품수량 & 버튼 -->
+      <div class="col">
+        <p>등록물품 : <span>2</span> 개</p>
+        <router-link :to="{ name: 'storeView' }">주문하기</router-link>
+      </div>
+      <hr style="border : 1px solid #222; margin: 3px">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "StoreItem",
+}
+</script>
+
+<style scoped>
+
+.store-container{
+  padding: 6px;
+}
+
+.store-container .row .store{
+  text-align: left;
+}
+.store-container .row .store .store-name{
+  font-size: 12px;
+  font-weight: bolder;
+}
+.store-container .row .store .store-prediction{
+  font-size: 6px !important;
+  color: #B9B9B9;
+}
+</style>
--- frontend/src/components/categoryStore.vue
+++ frontend/src/components/categoryStore.vue
@@ -31,18 +31,47 @@
       </div>
     </div>
     <!-- 검색 서칭 -->
-    <div>
-        <span>총 3 개 검색 결과</span>
-        <input type="text" name="" id="">
-        <button><i class="fa-regular fa-magnifying-glass"></i></button>
-    </div>
+    <div class="search-container">
+        <div class="row">
+          <div class="col-4">
+            <span class="search-result">총 3개 검색 결과</span>
+          </div>
+        
+          <div class="col">
+            <input class="search-box" type="search" placeholder=" 가게명을 입력해주세요">
+            <!-- 검색 아이콘 -->
+            <button class="product-search">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </button>
+            <!-- 초기화 -->
+            <button class="search-reset">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     <!-- 검색 결과 -->
+    <div>
+      <store-item></store-item>
+      <store-item></store-item>
+    </div>
   </div>
 </template>
 
 <script>
+import StoreItem from '@/components/StoreItem.vue'
+
 export default {
     name: "CategoryStore",
+
+    components:{
+      StoreItem,
+    }
 }
 </script>
 
@@ -61,4 +90,25 @@ export default {
   height: 70px;
  }
 
+.location {
+  text-align: left;
+  padding-bottom: 10px;
+}
+
+.search-container .row .col {
+  padding: 0;
+}
+.search-result{
+  padding-right: 5px; 
+  font-size: 12px;
+}
+.search-box{
+  border: solid 1px;
+  font-size: 12px
+}
+
+.product-search {
+  padding-right: 5px;
+  padding-left: 5px;
+}
 </style>
