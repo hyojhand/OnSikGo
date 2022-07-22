3b9e76298c4eac83aa12cf8ff93f2f5fb19f91d4
손효재 <sonjw5128@gmail.com>
2022-07-22 11:40:59 +0900

    add security ,jwt config

---
+++ backend/src/main/java/com/ssafy/onsikgo/config/SecurityConfig.java
@@ -0,0 +1,80 @@
+package com.ssafy.onsikgo.config;
+
+import com.ssafy.onsikgo.entity.Role;
+import com.ssafy.onsikgo.security.JwtAccessDeniedHandler;
+import com.ssafy.onsikgo.security.JwtAuthenticationEntryPoint;
+import com.ssafy.onsikgo.security.JwtSecurityConfig;
+import com.ssafy.onsikgo.security.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메서드 단위로 추가하기위해 적용
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                // exception을 핸들링할때 만들었던 에러처리 클래스들을 추가해준다.
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                /**
+                 * security
+                 */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/naver/login").permitAll()
+                .antMatchers("/board/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+
+                // 인증없이 접근을 허용하는 path 추가
+//                .and()
+//                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                // 나머지 요청은 모두 인증을 받는다
+                .anyRequest().authenticated()
+
+
+                // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig 클래스도 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
+}
