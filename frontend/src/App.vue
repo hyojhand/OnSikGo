d6fb0533e469505b35c496804f5e26d99f40db0f
jangchangwan <tktkd6755@gmail.com>
2022-07-26 10:34:29 +0900

    update nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,73 @@
 <template>
-  <v-app id="app">
-    <div class="margin">
-    <nav-bar id="nav" />
-    <router-view />
-    </div>
+  <v-app id="app" class="margin">
+
+    <v-app-bar absolute temporary dense color="#ffffff">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+
+      </v-app-bar-nav-icon>
+      
+      <v-spacer></v-spacer>
+      <v-btn icon color="white">
+        <v-icon>mdi-battery-charging</v-icon>
+      </v-btn>
+      <router-link 
+        :to="{ name: 'main' }">
+        <img 
+          src="@/assets/logo.jpg" 
+          alt="logo였던것.." 
+          style="height: 20%;
+                width: 20%;
+                margin: 3% 0%;"/>
+      </router-link>
+      <router-link 
+        :to="{ name: 'notice' }" 
+        class="col-sm"
+      >
+      <i class="fa-solid fa-bell"></i>
+      </router-link>
+    </v-app-bar>
+    <!-- 툴바 켰을때-->
+    <v-navigation-drawer app v-model="drawer" absolute clipped temporary>
+      <router-link 
+        :to="{ name: 'main' }">
+        <img 
+          src="@/assets/logo.jpg" 
+          alt="logo였던것.." 
+          style="height: 6%;
+                width: 20%;
+                margin: 3% 0%;"/>
+      </router-link>
+      <v-list nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.router">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view />
+    </v-content>
+
   </v-app>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+
 
 export default {
-  components: {
-    NavBar,
+
+  data () {
+    return {
+      drawer: false,
+      items: [
+        { title: '홈', router: '/'},
+        { title: '마이페이지', router: 'mypage/owner'},
+        { title: '온식고', router: 'shop'},
+        { title: '전체상품', router: 'allprod'},
+        { title: '기부', router: 'donation'},
+      ],
+    }
   },
 };
 </script>
@@ -34,7 +89,9 @@ export default {
   margin-bottom: 5%;
 }
 .margin {
-  margin: 0 5% 5% 5%;
+  margin: 5% 5% 5% 5%;
 
 }
+
+
 </style>
