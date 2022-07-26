23f215f9be105f25127bc846dca444a3f00c0dc4
손효재 <sonjw5128@gmail.com>
2022-07-26 11:42:01 +0900

    add item management

--- backend/src/main/java/com/ssafy/onsikgo/service/StoreService.java
+++ backend/src/main/java/com/ssafy/onsikgo/service/StoreService.java
@@ -38,7 +38,6 @@ public class StoreService {
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
 
-
     @Transactional
     public ResponseEntity<String> register(HttpServletRequest request, StoreDto storeDto) {
 
@@ -67,9 +66,6 @@ public class StoreService {
         if (!tokenProvider.validateToken(token)) {
             return new ResponseEntity<>("유효하지 않는 토큰", HttpStatus.NO_CONTENT);
         }
-
-        String userEmail = String.valueOf(tokenProvider.getPayload(token).get("sub"));
-        User findUser = userRepository.findByEmail(userEmail).get();
 
         Store findStore = storeRepository.findById(store_id).get();
 
