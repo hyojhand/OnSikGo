ce752a5614d1715595f3dabbe6361b68c1b75d7d
이동근 <ehdrms1426@gmail.com>
2022-07-27 11:25:13 +0900

    add ownerInfoChange page

--- frontend/src/views/OwnerInfoChangeView.vue
+++ frontend/src/views/OwnerInfoChangeView.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>
-    <h1>점주 회원정보 변경</h1>
+  <div class="container">
+    <div class="mail-box">
+      <div class="text-h5">이메일</div>
+      <input type="text" placeholder="ganadara@mabasa.com" />
+    </div>
+    <div class="password-box">
+      <div class="text-h5">비밀번호</div>
+      <v-btn @click="gochange()">수정 </v-btn>
+    </div>
+    <v-divider></v-divider>
+    <v-btn class="btn" justify="end" @click="gomypage()">수정 완료</v-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "OwnerInfoChangeView",
+  methods: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  min-width: 310px;
+  padding: 10%;
+}
+.mail-box {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.password-box {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  box-sizing: inherit;
+}
+</style>
