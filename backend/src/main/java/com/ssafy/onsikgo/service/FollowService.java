ec549b509e698a162b41393cefb8179dbe94ad18
공지훈 <wlgns3914@naver.com>
2022-07-28 15:09:24 +0900

    update follow management

--- backend/src/main/java/com/ssafy/onsikgo/service/FollowService.java
+++ backend/src/main/java/com/ssafy/onsikgo/service/FollowService.java
@@ -1,9 +1,7 @@
 package com.ssafy.onsikgo.service;
 
-import com.ssafy.onsikgo.dto.ReviewDto;
 import com.ssafy.onsikgo.dto.StoreDto;
 import com.ssafy.onsikgo.entity.Follow;
-import com.ssafy.onsikgo.entity.Review;
 import com.ssafy.onsikgo.entity.Store;
 import com.ssafy.onsikgo.entity.User;
 import com.ssafy.onsikgo.repository.FollowRepository;
@@ -20,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -70,5 +69,24 @@ public class FollowService {
             result.add(follow.getStore().toDto());
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+    @Transactional
+    public ResponseEntity<Object>delete(HttpServletRequest request, Long store_id){
+        String token = request.getHeader("access-token");
+        User findUser = null;
+        if (!tokenProvider.validateToken(token)) {
+            return new ResponseEntity<>("유효하지 않는 토큰", HttpStatus.NO_CONTENT);
+        }
+        String userEmail = String.valueOf(tokenProvider.getPayload(token).get("sub"));
+
+        findUser = userRepository.findByEmail(userEmail).get();
+
+        Store findStore = storeRepository.findById(store_id).get();
+
+        Follow follow = followRepository.findFollowByUserAndStore(findUser,findStore);
+
+        followRepository.delete(follow);
+
+        return new ResponseEntity<>("팔로우 삭제", HttpStatus.OK);
     }
 }
