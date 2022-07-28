7659e3a6998228447737adf219d71210d10739ce
공지훈 <wlgns3914@naver.com>
2022-07-28 11:25:59 +0900

    add review management

--- backend/src/main/java/com/ssafy/onsikgo/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/onsikgo/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sale/**").permitAll()
                 .antMatchers("/api/hello").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-
+                .antMatchers("/review/**").permitAll()
                 // 인증없이 접근을 허용하는 path 추가
 //                .and()
 //                .authorizeRequests()
---
+++ backend/src/main/java/com/ssafy/onsikgo/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.onsikgo.repository;
+
+import com.ssafy.onsikgo.entity.Review;
+import com.ssafy.onsikgo.entity.Store;
+import com.ssafy.onsikgo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review,Long> {
+}
