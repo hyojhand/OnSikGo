944ae2a895ee143ab7eabde4c838bfa5cf4229e6
손효재 <sonjw5128@gmail.com>
2022-07-31 19:36:03 +0900

    add store & user reivew

--- frontend/src/views/profile/MyreviewView.vue
+++ frontend/src/views/profile/MyreviewView.vue
@@ -3,9 +3,14 @@
     <img src="@/assets/images/solo.jpg" width="40" height="40" />
     <br />
 
-    <span> {{ userDto.userName }} 님의 리뷰</span>
+    <span> {{ reviewList[0].nickname }} 님의 리뷰</span>
     <hr />
-    <reviewList></reviewList>
+    <h5>🙋‍♀️🙋‍♂️ 리뷰 내역</h5>
+    <reviewList
+      v-for="(review, index) in reviewList"
+      :key="index"
+      v-bind="review"
+    />
   </div>
 </template>
 
@@ -19,15 +24,24 @@ export default {
   },
   data() {
     return {
-      userDto: {},
+      reviewList: [],
     };
   },
   created() {
-    http.defaults.headers["access-token"] =
-      localStorage.getItem("access-token");
-    http.get("/user").then((response) => {
-      this.userDto = response.data;
-    });
+    http
+      .post("/review/user", {
+        nickname: this.$route.params.nickname,
+      })
+      .then((response) => {
+        console.log(this.reviewList);
+        if (response.status == 200) {
+          if (response.data != null) {
+            this.reviewList = response.data;
+          } else {
+            alert("리뷰가 없습니다.");
+          }
+        }
+      });
   },
 };
 </script>
