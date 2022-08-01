4c94c0560130d9f6dd6062f4433e0eca41013426
jangchangwan <tktkd6755@gmail.com>
2022-08-01 17:52:30 +0900

    add vuex

--- frontend/src/store/modules/accouts.js
+++ frontend/src/store/modules/accouts.js
@@ -1,12 +1,20 @@
 const accounts = {
     state: {
+        currentAddress:"",
+
     },
     getters: {
 
     },
     actions: {
+        getAddress :({commit}, address) => {
+            commit("GET_ADDRESS", address)
+        },
     },
     mutations: {
+        GET_ADDRESS: (state, address) => {
+            state.currentAddress = address;
+        }
     },
   };
   
