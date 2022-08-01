b7709e9177e448fdac7a5831bb1d45f69bfed340
jangchangwan <tktkd6755@gmail.com>
2022-08-02 00:32:35 +0900

    update shop

--- frontend/src/components/home/MainStore.vue
+++ frontend/src/components/home/MainStore.vue
@@ -38,10 +38,16 @@
 </template>
 
 <script>
-
+import {mapGetters} from 'vuex';
 
 export default {
-    name: "CategoryStore",
+  name: "CategoryStore",
+
+  computed: {
+    ...mapGetters("store",[
+    "currentAddress"
+  ]),
+  }
 }
 </script>
 
--- frontend/src/components/home/SloganPage.vue
+++ frontend/src/components/home/SloganPage.vue
@@ -6,7 +6,7 @@
         <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"/>
         <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
       </svg>
-      <span> 서구 임시수도기념로 37, 구덕로 119길</span>
+      <span> {{ currentAddress }}</span>
     </div>
 
     <!-- 슬로건 -->
@@ -70,12 +70,20 @@
   </div>
 </template>
 <script>
+import { mapGetters } from "vuex";
+
+
 export default {
   name: "SloganPage",
   data() {
     return {
       slide: 0,
     };
+  },
+  computed: {
+  ...mapGetters("store",[
+    "currentAddress"
+  ]),
   },
   methods: {
     onSlideStart() {
