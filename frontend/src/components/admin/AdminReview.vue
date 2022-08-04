f8526dc212cac4ccd0a5c74ce2ba98c864761712
김가흔 <rkgms0712@naver.com>
2022-08-04 16:57:13 +0900

    adminReview

--- frontend/src/components/admin/AdminReview.vue
+++ frontend/src/components/admin/AdminReview.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <h1>AdminReview</h1>
+    <h1 class="mb-5">AdminReview</h1>
     <div
       v-for="(review, index) in reviewList"
       :key="index"
       class="row fs-5">
-        {{ review.nickname }} : {{ review.content }}
-        <br>
+      <div class="text-start">
+        <p class="text-primary">{{ review.nickname }} : {{ review.content }}</p>
+        <p>작성한 가게명: {{ review.storeDto.storeName }}</p>
+      </div>
         <v-btn color="error" samll depressed>삭제</v-btn>
         <hr>
     </div>
