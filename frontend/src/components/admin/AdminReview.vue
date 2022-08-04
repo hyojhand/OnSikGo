7f3b4a5b72fac468b40febee7495c2d3a80c3a56
김가흔 <rkgms0712@naver.com>
2022-08-04 15:17:25 +0900

    update adminReview

--- frontend/src/components/admin/AdminReview.vue
+++ frontend/src/components/admin/AdminReview.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <h1>AdminReview</h1>
+    <div
+      v-for="(reviewDto, index) in reviewList"
+      :key="index"
+      v-bind="reviewDto">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name : "AdminReview",
-}
+
+  data: function () {
+    return {
+      reviewList: [],
+    }
+  },
+};
 </script>
 
 <style>
