761f339b9497e224ccb502eedbfc645882696c76
김가흔 <rkgms0712@naver.com>
2022-08-06 23:04:30 +0900

    update login

--- frontend/src/views/accounts/LoginView.vue
+++ frontend/src/views/accounts/LoginView.vue
@@ -103,6 +103,7 @@ export default {
       this.$router.push("/signup");
     },
     login() {
+      this.loginCheck = false,
       http
         .post("/user/login", {
           email: this.email,
