ea78b2df19cd6d808d916e482b61eb4173b11cf0
jangchangwan <tktkd6755@gmail.com>
2022-08-17 10:53:36 +0900

    refactor router

--- frontend/src/views/accounts/LoginView.vue
+++ frontend/src/views/accounts/LoginView.vue
@@ -155,7 +155,7 @@ export default {
           .then((response) => {
             if (response.status == 200) {
               localStorage.setItem("access-token", response.data.token);
-              this.$router.push("/");
+              this.$router.push("/").catch(() => {});
             } else {
               this.loginCheck = true;
             }
